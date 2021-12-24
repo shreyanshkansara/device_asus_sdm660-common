aca71d6f695c97576d48ac2b0f0b6128e40371a4
RONIN808 <shubhamshedage808@gmail.com>
2021-12-24 14:29:11 +0530

    sdm660-common: build libwpa_client
    CANNOT LINK EXECUTABLE "lowi-server": library "libwpa_client.so" not found: needed by main executable
    CANNOT LINK EXECUTABLE "/vendor/bin/imsdatadaemon": library "libwpa_client.so" not found: needed by main executable
    CANNOT LINK EXECUTABLE "/system/vendor/bin/cnd": library "libwpa_client.so" not found: needed by /vendor/lib64/libcne.so in namespace (default)

--- sdm660.mk
+++ sdm660.mk
@@ -439,6 +439,7 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
     libwifi-hal-qcom \
+    libwpa_client \
     TetheringConfigOverlay \
     libwpa_client \
     WifiOverlay \
